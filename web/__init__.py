8b43dce7b5efb1e83cc49f0e39530000e2086084
Anni-065 <anni.leontarakis@gmail.com>
2025-08-31 16:18:49 +0200

    remove outdated web interface

--- web/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-MKV Manager Web Interface Module
-
-This module contains the Flask web application for the MKV Manager,
-providing a user-friendly interface for processing MKV files.
-"""
-
-__version__ = "1.0.0"
